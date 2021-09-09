6e98ac28b7b61d3786b5bc7a0d82696dd50f0a61
Joe Christensen <joechristensenwork@gmail.com>
2021-09-09 10:15:07 -0700

    commit tree a8b57dd788af5fdcec4510270ca5d24b59c3bba2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
+  before_action :authorize
 
+  private
+
+  def unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def authorize
+    @user = User.find_by(id: session[:user_id])
+    return render json: { errors: ["Not Authorized"] }, status: :unauthorized unless @user
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.create(user_id: 1, title: "ttt", instructions: "adbdfdfgfgdfgdfgdfgdfgdfgdfgdfgdfvdfvfvsssdsdasdacaxczxvbgbfghdfgsdvsvdsvada", minutes_to_complete: 32)
